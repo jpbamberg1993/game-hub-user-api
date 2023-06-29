add274fb451840a2cecdfcd2862aa38abb6bb2d6
J Paul Bamberg <jpbamberg1993@gmail.com>
2023-06-29 07:20:11 -0500

    Update schema validation method to not depend on try catch

--- src/middleware/schema-validation-middleware.ts
+++ src/middleware/schema-validation-middleware.ts
@@ -3,11 +3,10 @@ import { NextFunction, Request, Response } from 'express'
 
 export function schemaValidationMiddleware<T>(schema: ZodSchema<T>) {
 	return function validate(req: Request, res: Response, next: NextFunction) {
-		try {
-			schema.parse(req.body)
-			next()
-		} catch (e: any) {
-			return res.status(400).send(e.issues)
+		const response = schema.safeParse(req.body)
+		if (!response.success) {
+			return res.status(400).json({ error: response.error })
 		}
+		next()
 	}
 }
