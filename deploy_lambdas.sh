0eaf1540d78013059116be5746225f1b5f43ae83
J Paul Bamberg <jpbamberg1993@gmail.com>
2023-07-03 13:01:35 -0500

    Enable secure secrets pulled from s3

---
+++ deploy_lambdas.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+env=$1
+
+#Safety Checks
+
+if [ -z "$env" ] || ([ "$env" != "dev" ] && [ "$env" != "int" ])
+then
+	echo "Please provide arguments: Usage: ./deploy_lambdas.sh [ENV_TO_DEPLOY]"
+	echo "Supported environments: dev / int"
+	exit 1
+fi
+
+echo "Deployment started for environment: " $env
+
+#Creating necessary variables
+
+fileName="env.${env}.json"
+sourceLocation="s3://game-hub-secrets/${fileName}"
+
+#Downloading the config file from s3
+
+echo "Downloading from s3: " $sourceLocation
+aws s3 cp $sourceLocation $fileName
+
+#Deploying the lambdas via serverless
+
+echo "Deploying Lambdas"
+serverless deploy --stage $env
+echo "Lambdas deployed"
+
+#Print currrent environment
+
+sls print --stage $env
+
+#Once complete delete configs from local machine
+
+echo "Deleting config file from local"
+rm $fileName
+
+echo "Deployment complete for environment: " $env
+exit 0
