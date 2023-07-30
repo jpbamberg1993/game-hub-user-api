724aeeda1c37c01056d652fc372a0e55b4880a6f
J Paul Bamberg <jpbamberg1993@gmail.com>
2023-07-30 18:33:31 -0400

    Code cleanup

--- src/genres/genre.repository.ts
+++ src/genres/genre.repository.ts
@@ -3,14 +3,13 @@ import {
 	QueryCommandInput,
 	QueryCommand,
 } from '@aws-sdk/client-dynamodb'
-import { marshall, unmarshall } from '../../dynamodb/dynamo-db'
+import { marshall, unmarshall } from '@aws-sdk/util-dynamodb'
 import { Genre } from './genre.schema'
 
 export class GenreRepository {
 	constructor(private readonly ddbDocClient: DynamoDBClient) {}
 
 	async getAll() {
-		console.log(`--> [GenreRepository] getAll`)
 		const params: QueryCommandInput = {
 			TableName: process.env.DYNAMODB_TABLE,
 			KeyConditionExpression: `#entityType = :entityType`,
@@ -24,7 +23,6 @@ export class GenreRepository {
 		}
 		const command = new QueryCommand(params)
 		const results = await this.ddbDocClient.send(command)
-		console.log({ results })
 		const genres = results.Items?.map((item) => unmarshall(item)) as Genre[]
 		return { genres }
 	}
